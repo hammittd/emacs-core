23858bb8d14766d6c89c423ff6d474909bffa023
Derek Hammitt <derekrhammitt@gmail.com>
2020-03-10 05:05:14 +0000

    Add emacs-custom.el custom-file

--- init.el
+++ init.el
@@ -39,15 +39,7 @@
 ;; User Interface Settings
 (require 'core-ui)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (zenburn-theme projectile smartparens ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Set custom-file
+(setq custom-file (expand-file-name "emacs-custom.el" custom-dir))
+(if (file-exists-p custom-file)
+    (load custom-file))
