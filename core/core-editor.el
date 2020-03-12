1d9ec38cc150aa27dd4526c1d7cee08bb742ea0c
Derek Hammitt <derekrhammitt@gmail.com>
2020-03-12 04:22:41 +0000

    Alphabetize settings, add ivy settings and keybindings

--- core/core-editor.el
+++ core/core-editor.el
@@ -2,7 +2,14 @@
 ;;
 ;; Derek Hammitt
 ;; 2020-03-10
+;;
+;; Description:
+;; Utilities, settings, and keybindings for the editor.
+;; Also includes two fuctions that will use all Settings and Keybindings
+;;
+;; Code:
 
+;; Utilities
 (defun comment-or-uncomment-region-or-line ()
   "Comments or uncomments a region or the current line if there's no region."
   (interactive)
@@ -21,6 +28,20 @@
   (save-some-buffers t))
 
 ;; Settings
+(defun activate-ivy-mode ()
+  (ivy-mode 1))
+
+(defun always-highlight-code ()
+  "Syntax highlight where possible."
+  (global-font-lock-mode t))
+
+(defun append-newline ()
+  (setq require-final-newline t))
+
+(defun apply-changes-to-highlighted-region ()
+  "Transient mark mode."
+  (transient-mark-mode t))
+
 (defun auto-save-on ()
   (add-hook 'text-mode-hook #'auto-save-visited-mode)
   (setq auto-save-timeout 1)
@@ -30,12 +51,8 @@
   "Call DELETE-TRAILING-WHITESPACE when a buffer is saved."
   (add-hook 'before-save-hook 'delete-trailing-whitespace))
 
-(defun treat-camelcase-as-separate-words ()
-  (add-hook 'prog-mode-hook 'subword-mode))
-
-(defun save-scripts-as-executable ()
-  (add-hook 'after-save-hook
-	    'executable-make-buffer-file-executable-if-script-p))
+(defun flash-instead-of-bell ()
+  (setq visible-bell t))
 
 (defun offer-to-create-dirs-on-save ()
   (add-hook 'before-save-hook
@@ -45,47 +62,30 @@
 		  (when (and (not (file-exists-p dir))
 			     (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
 		    (make-directory dir t)))))))
-;;
-(defun apply-changes-to-highlighted-region ()
-  "Transient mark mode."
-  (transient-mark-mode t))
 
 (defun overwrite-selection ()
   (delete-selection-mode t))
-
-(defun append-newline ()
-  (setq require-final-newline t))
 
 (defun quiet-startup ()
   (setq inhibit-startup-message t)
   (setq initial-scratch-message nil))
 
-(defun shorten-yes-or-no ()
-  (fset 'yes-or-no-p 'y-or-n-p))
-
-(defun always-highlight-code ()
-  "Syntax highlight where possible."
-  (global-font-lock-mode t))
-
 (defun refresh-buffers-when-files-change ()
   (global-auto-revert-mode t))
+
+(defun save-scripts-as-executable ()
+  (add-hook 'after-save-hook
+	    'executable-make-buffer-file-executable-if-script-p))
+
+(defun shorten-yes-or-no ()
+  (fset 'yes-or-no-p 'y-or-n-p))
 
 (defun show-matching-parens ()
   (show-paren-mode t)
   (setq show-paren-delay 0.0))
 
-(defun flash-instead-of-bell ()
-  (setq visible-bell t))
-
 (defun set-default-line-length-to (line-length)
   (setq-default fill-column line-length))
-
-(defun spaces-not-tabs ()
-  (setq-default indent-tabs-mode nil)
-  (setq-default tab-width 8))
-
-(defun tab-autocomplete ()
-  (setq tab-always-indent 'complete))
 
 (defun smartparens-config ()
   (require 'smartparens-config)
@@ -95,7 +95,19 @@
   (sp-use-paredit-bindings)
   (show-smartparens-global-mode +1))
 
+(defun spaces-not-tabs ()
+  (setq-default indent-tabs-mode nil)
+  (setq-default tab-width 8))
+
+(defun tab-autocomplete ()
+  (setq tab-always-indent 'complete))
+
+(defun treat-camelcase-as-separate-words ()
+  (add-hook 'prog-mode-hook 'subword-mode))
+
+;; Use all the settings:
 (defun all-settings ()
+  (activate-ivy-mode)
   (auto-save-on)
   (delete-trailing-whitespace)
   (treat-camelcase-as-separate-words)
@@ -120,6 +132,9 @@
   (global-set-key (kbd "M-;")
 		  'comment-or-uncomment-region-or-line))
 
+(defun bind-ibuffer-instead-of-buffer-menu ()
+  (global-set-key (kbd "C-x C-b") 'ibuffer))
+
 (defun bind-keys-to-change-text-size ()
   "Bind C-+ and C-- to increase and decrease text size,
 respectively."
@@ -129,9 +144,14 @@ respectively."
   (define-key global-map (kbd "C-_") 'text-scale-decrease)
   (define-key global-map (kbd "C--") 'text-scale-decrease))
 
+(defun use-ivy-isearch ()
+  (global-set-key (kbd "C-s") 'swiper-isearch))
+
 (defun use-all-keybindings ()
   (bind-comment-uncomment-to-meta-semi)
-  (bind-keys-to-change-text-size))
+  (bind-ibuffer-instead-of-buffer-menu)
+  (bind-keys-to-change-text-size)
+  (use-ivy-isearch))
 
 ;; Backup to /tmp directory
 (defun backup-to-tmp ()
